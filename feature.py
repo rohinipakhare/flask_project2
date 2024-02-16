75c6683aa1240a437b3952999b4ce907623f2294
rohinipakhare <rohinipakhare30@gmail.com>
2024-02-16 18:55:18 +0530

    first commit

---
+++ feature.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+
+
+FAI=Flask(__name__)
+
+@FAI.route('/send_html')
+def send_html():
+    return render_template('send_html.html',name='ROHINI') 
+
+@FAI.route('/properties')
+def properties():
+    return render_template('properties.html')
+
+
+if __name__=='__main__':
+    FAI.run(debug=True)
